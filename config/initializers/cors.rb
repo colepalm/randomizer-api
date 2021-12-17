79989e89df44b364505b65c0fdd2cb3e86943532
colepalm <palm.cole@gmail.com>
2021-12-16 20:32:06 -0700

    add cors gem

---
+++ config/initializers/cors.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://playlist-randomizer-f2a33.firebaseapp.com/:80'
+    resource '*', headers: :any, methods: [:get, :post]
+  end
+end
